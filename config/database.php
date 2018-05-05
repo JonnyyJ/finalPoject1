8f094a2110d8d400d9f32a6bc631f3e4bb6adb27
jonnyy <jc958@njit.edu>
2018-05-05 15:01:58 -0700

    Configure the running environment for laravel dusk

--- config/database.php
+++ config/database.php
@@ -114,17 +114,18 @@ return [
             'port' => env('REDIS_PORT', 6379),
             'database' => 0,
         ],
-        'pg-heroku' => [
-            'driver' => 'pgsql',
-            'host' => $heroku_db_url['host'],
-            'database' => substr($heroku_db_url['path'], 1),
-            'username' => $heroku_db_url['user'],
-            'password' => $heroku_db_url['pass'],
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-        ],
 
+
+    ],
+    'pg-heroku' => [
+        'driver' => 'pgsql',
+        'host' => $heroku_db_url['host'],
+        'database' => substr($heroku_db_url['path'], 1),
+        'username' => $heroku_db_url['user'],
+        'password' => $heroku_db_url['pass'],
+        'charset' => 'utf8',
+        'prefix' => '',
+        'schema' => 'public',
     ],
 
 ];
